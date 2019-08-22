bfe2c3bf37a27934a6d91d8a5d2d4cf221b3e861
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-08-22 10:42:40 -0400

    feat: create html page, reorg county map files

--- LWCF/GrantMap/County_Map_Script.js
+++ LWCF/GrantMap/County_Map_Script.js
@@ -1,39 +1,41 @@
-// New England County Map
+// LWCF Grant Amounts by County Map
 
 // Path to topoJSON county data and grant data
 mapPath = 'https://cdn.jsdelivr.net/npm/us-atlas@2/us/10m.json'
 grantPath = './Data/county-records.json'
 
+const format = d3.format(",");
 const path = d3.geoPath();
 
-const svg = d3.create("svg")
+const svg = d3.select("#map")
+  .append("svg")
   .attr("viewBox", "0 0 960 600")
   .style("width", "100%")
   .style("height", "auto");
 
+// Add group for legend
 // svg.append("g")
 //   .attr("transform", "translate(600,40)");
 //  .call(legend);
 
-const format = d3.format(",");
-
-// Create requests to retrieve JSON topography data and CSV population data
+// Create requests to retrieve JSON topography and grant data
 const getMapData = d3.json(mapPath);
 const getGrantData = d3.json(grantPath);
 
 Promise.all([getMapData, getGrantData]).then(function(values) {
-  const json = values[0];
+  const map = values[0];
   const grants = values[1];
 
 
-  const counties = topojson.feature(json, json.objects.countries).features;
-  const states = topojson.mesh(json, json.objects.states, (a, b) => a != b);
+  const counties = topojson.feature(map, map.objects.countries).features;
+  const states = topojson.mesh(map, map.objects.states, (a, b) => a != b);
 
   // Tooltip setup
   // const tooltip = d3.select("#tooltip")
   //     .style("display", "none")
   //     .classed("tooltip", true);
 
+  // Create county map
   svg.append("g")
   .selectAll("path")
   .data(counties)
@@ -44,6 +46,7 @@ Promise.all([getMapData, getGrantData]).then(function(values) {
     .text(d => `${d.properties.name}, ${states.get(d.id.slice(0, 2)).name}
   ${format(data.get(d.id))}`);
 
+  // Add state outlines
   svg.append("path")
     .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
     .attr("fill", "none")
