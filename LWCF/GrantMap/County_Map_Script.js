0fa427c154e51b248c5672553a0c230050c27698
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-08-22 11:25:43 -0400

    feat: update script

--- LWCF/GrantMap/County_Map_Script.js
+++ LWCF/GrantMap/County_Map_Script.js
@@ -1,8 +1,8 @@
 // LWCF Grant Amounts by County Map
 
 // Path to topoJSON county data and grant data
-mapPath = 'https://cdn.jsdelivr.net/npm/us-atlas@2/us/10m.json'
-grantPath = './Data/county-records.json'
+mapPath = 'https://cdn.jsdelivr.net/npm/us-atlas@2/us/10m.json';
+grantPath = '../Data/county-records.json';  // TODO: Update with all counties
 
 const format = d3.format(",");
 const path = d3.geoPath();
@@ -28,7 +28,7 @@ Promise.all([getMapData, getGrantData]).then(function(values) {
 
 
   const counties = topojson.feature(map, map.objects.countries).features;
-  const states = topojson.mesh(map, map.objects.states, (a, b) => a != b);
+  const states = topojson.mesh(map, map.objects.states, (a, b) => a !== b);
 
   // Tooltip setup
   // const tooltip = d3.select("#tooltip")
@@ -43,12 +43,13 @@ Promise.all([getMapData, getGrantData]).then(function(values) {
     // .attr("fill", d => color(data.get(d.id)))
     .attr("d", path)
   .append("title")
-    .text(d => `${d.properties.name}, ${states.get(d.id.slice(0, 2)).name}
-  ${format(data.get(d.id))}`);
+    .text(d => `${d.properties.name}, ${states.get(d.id.slice(0, 2)).name}`);
+  // .text(d => `${d.properties.name}, ${states.get(d.id.slice(0, 2)).name}
+  // ${format(data.get(d.id))}`);
 
   // Add state outlines
   svg.append("path")
-    .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+    .datum(states)
     .attr("fill", "none")
     .attr("stroke", "white")
     .attr("stroke-linejoin", "round")
