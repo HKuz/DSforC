fd5a5ed3ec7c2e69fe220b07542320518492ce5c
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-08-26 18:52:39 -0400

    feat: calc stats, add legend to map

--- LWCF/GrantMap/County_Map_Script.js
+++ LWCF/GrantMap/County_Map_Script.js
@@ -6,7 +6,65 @@ grantPath = './us_grants_by_county.json';
 
 const format = d3.format("$,");
 const path = d3.geoPath();
-const color = d3.scaleQuantize([1, 10], d3.schemeGreens[9]);  // TODO: Update for max/min in data
+
+// Create a scale to map total grant value of a county to a color
+const color = d3.scaleThreshold()
+    .domain([
+      1000,
+      10000,
+      100000,
+      500000,
+      750000,
+      1000000,
+      10000000,
+      20000000])
+    .range(d3.schemeGreens[9]);
+
+const formatDict = {
+  1000: "$1K",
+  10000: "$10K",
+  100000: "$100K",
+  500000: "$500K",
+  750000: "$750K",
+  1000000: "$1M",
+  10000000: "$10M",
+  20000000: "$20M"
+};
+
+legend = g => {
+  const w = 260;
+  const length = color.range().length;
+
+  const x = d3.scaleLinear()
+      .domain([1, length - 1])
+      .rangeRound([w / length, w * (length - 1) / length]);
+
+  // Draw boxes for each color threshold
+  g.selectAll("rect")
+    .data(color.range())
+    .join("rect")
+      .attr("x", (d, i) => x(i))
+      .attr("width", w / length)
+      .attr("height", 8)
+      .attr("fill", d => d);
+
+  // Add legend title
+  g.append("text")
+      .attr("y", -6)
+      .attr("fill", "currentColor")
+      .attr("text-anchor", "start")
+      .attr("font-weight", "bold")
+      .text("Total Value of LWCF Grants ($)");
+
+  // Draw ticks and labels
+  g.call(d3.axisBottom(x)
+      .tickSize(10)
+      .ticks(length - 1)
+      .tickFormat(i => formatDict[color.domain()[i - 1]])
+      )
+    .select(".domain")
+      .remove();
+}
 
 const svg = d3.select("#map")
   .append("svg")
@@ -14,10 +72,10 @@ const svg = d3.select("#map")
   .style("width", "100%")
   .style("height", "auto");
 
-// Add group for legend
-// svg.append("g")
-//   .attr("transform", "translate(600,40)");
-//  .call(legend);
+// Add group to hold the legend
+svg.append("g")
+  .attr("transform", "translate(600,40)")
+  .call(legend);
 
 // Create requests to retrieve JSON topography and grant data
 const getMapData = d3.json(mapPath);
@@ -41,7 +99,7 @@ Promise.all([getMapData, getGrantData]).then(function(values) {
     .join("path")
       .attr("fill", d => {
         if (grants[d.id]) {
-          return color(grants[d.id]['grant_value']);
+          return color(+grants[d.id]['grant_value']);
         } else {
           return "gray"
         }
